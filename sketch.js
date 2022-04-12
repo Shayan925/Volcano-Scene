7c0818306037a73845b4f5129d63dfa97f563480
Shayan <sjizan@outlook.com>
2022-04-11 21:04:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,7 @@
 /*
-Name: Shayan Jizan
+Name: Shayan
 Date: March 31st, 2022
 Description: Volcano Scene
-
-Criteria:
-- At least 3 different shapes used
-      circle, rectangle, quadrilateral, ellipse
-- At least 3 different colours used
-      yellow, red, brown, green, dark blue, light blue, white,   purple, black
-- One custom shape
-      Volcano 
-- Minimum of 6 elements
-      Sun, boat, waves, island, ocean, volcano, plane
-- At least 2 elements must move on their own
-      Boat, lava balls, waves, plane
-- Mouse interaction
-      clicking the screen will make the volcano erupt
-- Keyboard interaction
-      Using the left and right arrow key will move the sun and
-      change the colour of the sky
 */
 
 let clicked = false;
